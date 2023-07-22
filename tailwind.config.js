95dbe73c8111150e1bab7b853bffc677ce9a58d9
Debaditya Bhar <bhar.debaditya@gmail.com>
2023-07-22 21:14:45 +0530

    Version 1.0.0 - This is initial setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto Condensed", "sans-serif"],
+                righteous: ["Righteous", "cursive"],
+                poppins: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                "brand-color1": "#03045E",
+                "brand-color2": "#00B4D8",
+                "facebook-color": "#3b5998",
+                "insta-color": "#d6249f",
+                "github-color": "#f4a261",
+                "twitter-color": "#1da1f2",
+                "linkedin-color": "#0E76A8",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
