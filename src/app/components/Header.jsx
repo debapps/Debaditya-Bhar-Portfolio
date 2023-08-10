a6427401a6e9d79c9ffe1d5d79ba1cfb6f0e5245
Debaditya Bhar <bhar.debaditya@gmail.com>
2023-08-10 21:14:58 +0530

    Version 1.3.1 - fixed header positioning.

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,18 +7,20 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header
-            className="w-full px-20 py-8 bg-light-color dark:bg-dark-color 
-                flex flex-row justify-between items-center relative">
-            <NavBar links={navData} />
+            className="w-full px-20 py-16 bg-light-color dark:bg-dark-color 
+                flex justify-between items-center relative">
+            <div className="absolute left-5">
+                <NavBar links={navData} />
+            </div>
             <div className="absolute left-[50%] translate-x-[-50%]">
                 <Logo />
             </div>
-            <div className="lg:hidden block">
+            <div className="md:hidden block absolute right-5">
                 <SocialIcons />
             </div>
 
             <Link
-                className="hidden md:inline-block absolute right-2 p-2 rounded-full w-16 h-16 
+                className="hidden md:inline-block absolute right-5 p-2 rounded-full w-16 h-16 
                 text-center text-base font-righteous bg-dark-color text-light-color animate-pulse
                 dark:bg-secondary-color border-2 border-solid border-transparent hover:border-dark-color 
                 dark:hover:border-light-color hover:bg-light-color hover:text-dark-color"
